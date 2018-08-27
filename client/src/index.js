6b272fd660e6c79d96d212fbdc630c2bd07f7874
Alex Lin <alexlin311@gmail.com>
2018-08-26 17:04:50 -0700

    Removed some create-react-app default files.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MapSelector from './utils/MapSelector.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MapSelector />, document.getElementById('root'));
 registerServiceWorker();
